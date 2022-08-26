02ca02d0777a8250ba90782f252a0fa6640d1de5
Akito <mallueck@gmail.com>
2022-08-27 00:08:07 +0200

    adjust release profile

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 shell="$(getent passwd | grep $USER)"
 shell=${shell##*:}
 
-$shell -c 'cargo build --release && time ./target/release/rustsieve'
+$shell -c 'RUSTFLAGS="-C target-cpu=native" cargo build --release && time ./target/release/rustsieve'
